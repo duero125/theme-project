592e6c19e86908a3e556fdffde860dd275798387
duero <duero@wp.pl>
2019-08-17 20:30:54 +0200

    Zmieniono bazę z sqlite na postgres

--- theme/settings.py
+++ theme/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'theme.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'themedb',
+        'USER': 'postgres',
+        'PASSWORD': 'chrusrys',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
